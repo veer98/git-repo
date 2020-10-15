bff967a9ce5106130da613d5123f4834393e8a2a
veer98 <51856799+veer98@users.noreply.github.com>
2020-10-15 15:45:25 +0530

    first commit

---
+++ OnlineShopping/src/com/lti/model/Customer.java
@@ -0,0 +1,19 @@
+package com.lti.model;
+
+public class Customer {
+int cusId;
+String custName;
+public int getCusId() {
+	return cusId;
+}
+public void setCusId(int cusId) {
+	this.cusId = cusId;
+}
+public String getCustName() {
+	return custName;
+}
+public void setCustName(String custName) {
+	this.custName = custName;
+}
+
+}
