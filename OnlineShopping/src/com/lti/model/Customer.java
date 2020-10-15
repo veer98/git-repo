fb26d741069f4fe9876e5e42f008a9d81af02440
veer98 <51856799+veer98@users.noreply.github.com>
2020-10-15 15:49:40 +0530

    Made changes in cust class

--- OnlineShopping/src/com/lti/model/Customer.java
+++ OnlineShopping/src/com/lti/model/Customer.java
@@ -3,6 +3,13 @@ package com.lti.model;
 public class Customer {
 int cusId;
 String custName;
+String custCity;
+public String getCustCity() {
+	return custCity;
+}
+public void setCustCity(String custCity) {
+	this.custCity = custCity;
+}
 public int getCusId() {
 	return cusId;
 }
