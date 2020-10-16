45c7811c380ed12129dcaf32b6c656738eface55
veer98 <51856799+veer98@users.noreply.github.com>
2020-10-16 09:42:57 +0530

    added account class through br1

---
+++ OnlineShopping/src/com/lti/model/Account.java
@@ -0,0 +1,19 @@
+package com.lti.model;
+
+public class Account {
+	int accountId;
+	int balance;
+	public int getAccountId() {
+		return accountId;
+	}
+	public void setAccountId(int accountId) {
+		this.accountId = accountId;
+	}
+	public int getBalance() {
+		return balance;
+	}
+	public void setBalance(int balance) {
+		this.balance = balance;
+	}
+
+}
